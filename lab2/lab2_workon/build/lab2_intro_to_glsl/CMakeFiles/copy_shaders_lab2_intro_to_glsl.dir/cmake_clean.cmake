007e6ac914da5d408614b1eac62159ce59c1555e
roblan11 <roblan11@hotmail.ch>
2017-03-09 13:10:20 +0100

    remove labs from git

--- lab2/lab2_workon/build/lab2_intro_to_glsl/CMakeFiles/copy_shaders_lab2_intro_to_glsl.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_shaders_lab2_intro_to_glsl.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- lab2/lab2_workon/lab2_intro_to_glsl/triangle/triangle_fshader.glsl
+++
@@ -1,8 +0,0 @@
-#version 330
-
-in vec3 fcolor;
-out vec3 color; // output color
-
-void main() {
-    color = fcolor; // color is red for every fragment
-}
