8da9aa86bf2c1ea60ccf586d3e1b9029290880b7
roblan11 <roblan11@hotmail.ch>
2017-03-13 13:14:25 +0100

    hw3 ex3: remove cout returning value of y when zooming

--- hw3-Badoux-Lang-Mulakhel/hw2_trackball/main.cpp
+++ hw3-Badoux-Lang-Mulakhel/hw2_trackball/main.cpp
@@ -184,7 +184,6 @@ void MousePos(GLFWwindow* window, double x, double y) {
         // moving the mouse cursor up and down (along the screen's y axis)
         // should zoom out and it. For that you have to update the current
         // 'view_matrix' with a translation along the z axis.
-        std::cout << p[1] << '\n';
         mat4 view_zoom = translate(mat4(1.f), vec3(0.f, 0.f, p[1] / 10));
         view_matrix = view_matrix * view_zoom;
     }
