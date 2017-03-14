f599af2eea8e18873a8a81b5e965567b7f15fcdc
Ito <rehan.mulakhel@epfl.ch>
2017-03-14 17:15:01 +0100

    Correction of the last one (not the bonus)

--- hw3-Badoux-Lang-Mulakhel/hw2_trackball/grid/grid_vshader.glsl
+++ hw3-Badoux-Lang-Mulakhel/hw2_trackball/grid/grid_vshader.glsl
@@ -14,15 +14,12 @@ void main() {
     // plane.
     // TODO 6: animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
-    const float amplitude = 0.1;
-    const float waveSpeedX = 0.75;
-    const float waveSpeedY = 0.5;
-    const float omegaX = 2 * 3.14 * 2.2;
-    const float omegaY = 2 * 3.14 * 1.9;
-    float tmpX = uv.x * omegaX + waveSpeedX * time;
-    float tmpY = uv.y * omegaY + waveSpeedY * time;
-    float height = amplitude * sin(tmpX + tmpY);
+    const float amplitude = 0.06;
+    const float omega = 2 * 3.14 * 1.2;
+    float tmpX = uv.x * omega + time * 3;
+    float tmpY = uv.y * omega + time;
+    float height = amplitude * sin(tmpX) * sin(tmpY);
     vec3 pos_3d = vec3(position.x, height, -position.y);
-    
+
     gl_Position = MVP * vec4(pos_3d, 1.0);
 }
