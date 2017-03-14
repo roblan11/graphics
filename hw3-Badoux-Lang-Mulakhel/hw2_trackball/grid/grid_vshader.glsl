dd06989e1bb64cfe9144db2f7d734d2ebf7eec9a
Ito <rehan.mulakhel@epfl.ch>
2017-03-14 14:26:50 +0100

    The grid moves like waves. The parameters can easily be changed.

--- hw3-Badoux-Lang-Mulakhel/hw2_trackball/grid/grid_vshader.glsl
+++ hw3-Badoux-Lang-Mulakhel/hw2_trackball/grid/grid_vshader.glsl
@@ -14,8 +14,15 @@ void main() {
     // plane.
     // TODO 6: animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
-    float height = 0.0;
+    const float amplitude = 0.1;
+    const float waveSpeedX = 0.75;
+    const float waveSpeedY = 0.5;
+    const float omegaX = 2 * 3.14 * 2.2;
+    const float omegaY = 2 * 3.14 * 1.9;
+    float tmpX = uv.x * omegaX + waveSpeedX * time;
+    float tmpY = uv.y * omegaY + waveSpeedY * time;
+    float height = amplitude * sin(tmpX + tmpY);
     vec3 pos_3d = vec3(position.x, height, -position.y);
-
+    
     gl_Position = MVP * vec4(pos_3d, 1.0);
 }
