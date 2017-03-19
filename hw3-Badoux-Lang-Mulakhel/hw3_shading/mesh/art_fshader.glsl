0c50540a0218073106a8bb5eeafd46de36f50539
christophebx <ch.bx@bluewin.ch>
2017-03-19 01:26:17 +0100

    début 3.2

--- hw3-Badoux-Lang-Mulakhel/hw3_shading/mesh/art_fshader.glsl
+++ hw3-Badoux-Lang-Mulakhel/hw3_shading/mesh/art_fshader.glsl
@@ -1,5 +1,11 @@
 #version 330
 
+uniform vec3 La, Ld, Ls;
+uniform vec3 ka, kd, ks;
+
+in vec3 normal_mv;
+in vec3 light_dir;
+in vec3 view_dir;
 
 out vec3 color;
 
@@ -9,19 +15,20 @@ uniform sampler2D tex2D;
 void main() {
     color = vec3(0.0, 0.0, 0.0);
 
-    const vec3 COLORS[6] = vec3[](
-        vec3(1.0,0.0,0.0),
-        vec3(0.0,1.0,0.0),
-        vec3(0.0,0.0,1.0),
-        vec3(1.0,1.0,0.0),
-        vec3(0.0,1.0,1.0),
-        vec3(1.0,0.0,1.0));
-    int index = int( mod(gl_PrimitiveID,6) );
-    color = COLORS[index];
-
     //>>>>>>>>>> TODO >>>>>>>>>>>
     // TODO 3.2: Artistic shading.
     // 1) compute the output color by doing a look-up in the texture using the
     //    texture sampler tex.
     //<<<<<<<<<< TODO <<<<<<<<<<<
+
+    color = La*ka;
+    vec3 n = normalize(normal_mv);
+    vec3 l = normalize(light_dir);
+    vec3 v = normalize(view_dir);
+    vec3 r = reflect(-l,n);
+    float value = texture(tex2D, vec2(dot(n,l), pow(max(dot(r,v), 0.0), alpha))).x;
+    if(value > 0.0) {
+        color += Ld*kd*value;
+        color += Ls*ks*value;
+    }
 }
