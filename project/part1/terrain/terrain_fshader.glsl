3fe60e15e1ef666bf2589efa7b1448bd439413e8
roblan11 <roblan11@hotmail.ch>
2017-04-08 16:03:43 +0200

    fBm

--- project/part1/terrain/terrain_fshader.glsl
+++ project/part1/terrain/terrain_fshader.glsl
@@ -7,12 +7,14 @@ out vec3 color;
 uniform sampler2D tex;
 
 void main() {
-    float temp = texture(tex,uv).x;
-    if(temp < 0.1){
+    float temp = texture(tex, uv).x;
+    if(temp < 0.01){
         color = vec3(0, 0.6, 0.9);
-    } else if (temp < 0.7){
-        color = vec3(0.1, 0.7, 0.7);
+    } else if (temp < 0.05){
+        color = vec3(0.6, 0.7, 0.0);
+    } else if (temp < 0.3){
+        color = vec3(0.2, 0.7, 0.1);
     } else {
-        color = vec3(1.0, 1.0, 1.0);
+        color = vec3(0.9, 0.9, 0.9);
     }
 }
