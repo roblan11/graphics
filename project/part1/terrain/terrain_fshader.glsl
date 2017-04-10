fc4fd51fe8f4db9043889ba3b2a0abfbeffe30e8
roblan11 <roblan11@hotmail.ch>
2017-04-10 16:19:57 +0200

    interp colors

--- project/part1/terrain/terrain_fshader.glsl
+++ project/part1/terrain/terrain_fshader.glsl
@@ -8,12 +8,16 @@ uniform sampler2D tex;
 
 void main() {
     float temp = texture(tex, uv).x;
-    if(temp < 0.01){
+    if(temp < 0.001){
         color = vec3(0, 0.6, 0.9);
-    } else if (temp < 0.05){
+    } else if (temp < 0.02){
         color = vec3(0.6, 0.7, 0.0);
-    } else if (temp < 0.3){
+    } else if (temp < 0.07){
+        color = mix(vec3(0.6, 0.7, 0.0), vec3(0.2, 0.7, 0.1), (temp-0.02)/0.07);
+    } else if (temp < 0.2){
         color = vec3(0.2, 0.7, 0.1);
+    } else if (temp < 0.4){
+        color = mix(vec3(0.2, 0.7, 0.1), vec3(0.9, 0.9, 0.9), (temp-0.2)/0.2);
     } else {
         color = vec3(0.9, 0.9, 0.9);
     }
